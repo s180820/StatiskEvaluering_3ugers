523948296ae01f97f4aaabc72df12b2e2ad655a4
Kat <s183910@student.dtu.dk>
2020-01-13 14:23:22 +0100

    her

--- Projekt1.r
+++ Projekt1.r
@@ -1,5 +1,20 @@
+#Project 1
 
+#Load data from experiment 3
 d <- armdata[3]
 print(d)
+
+repetitions <- 10
+participants <- 10
+experiments <- 16
+
+y <- as.matrix(read.table('armTrajectories.dat'))
+dim(y) <- c(nrow(y), 3, repetitions , participants , experiments)
+
+yvelo <- as.matrix(read.table('armVelocity.dat'))
+dim(yvelo) <- c(nrow(yvelo), 3, repetitions , participants , experiments)
+
+
+
 
 
